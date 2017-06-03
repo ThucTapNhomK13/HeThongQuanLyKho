7bcea9c4178e877cec2afe99b0fbed34197b3d1a
4un9cu0ng <3h08mi@gmail.com>
2017-06-03 18:38:09 +0700

    xxx

--- HeThongQuanLyKho/HeThongQuanLyKho/Program.cs
+++ HeThongQuanLyKho/HeThongQuanLyKho/Program.cs
@@ -16,7 +16,7 @@ namespace HeThongQuanLyKho
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new fNhapXuat());
+            Application.Run(new fDangNhap());
         }
     }
 }
